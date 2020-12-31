bb8f09456f8ea02ef75d849ea9ef42faaa79329e
Anna Kaser <kaserann96@gmail.com>
2020-12-31 15:51:14 -0700

    login and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,16 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type Thought {
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -19,22 +26,22 @@ type Thought {
     username: String
   }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-
   type Query {
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
